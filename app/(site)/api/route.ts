e66f0ff629a757200774a759f7f45a2363628330
Hai Nguyen <namhainguyenminh@gmail.com>
2023-12-31 14:26:35 -0600

    change eslint file and api routes

--- app/(site)/api/route.ts
+++ app/(site)/api/route.ts
@@ -9,16 +9,21 @@ import { EmailTemplate } from "@/components/email-template"
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY)
 const email = process.env.NEXT_PUBLIC_EMAIL!
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { userEmail, message } = await req.body
   try {
-    const data = await resend.emails.send({
-      from: email,
-      to: email,
-      subject: "Want To Connect Email From User",
-      react: EmailTemplate({ userEmail, message }) as React.ReactElement,
-    })
-    return NextResponse.json(data)
+    if (req.method === "POST") {
+      const data = await resend.emails.send({
+        from: email,
+        to: email,
+        subject: "Want To Connect Email From User",
+        react: EmailTemplate({ userEmail, message }) as React.ReactElement,
+      })
+      return NextResponse.json(data)
+    }
   } catch (error) {
     return NextResponse.json({ error })
   }
